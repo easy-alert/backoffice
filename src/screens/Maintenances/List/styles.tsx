a784eae381248615688dca855c008cafe37bc7c5
jorgelda <jorgeluiz112233@gmail.com>
2023-02-17 09:05:19 -0300

    develop -

--- src/screens/Maintenances/List/styles.tsx
+++ src/screens/Maintenances/List/styles.tsx
@@ -105,5 +105,12 @@ export const CategoriesContainer = styled.div`
   width: 100%;
   overflow-x: auto;
   overflow-y: hidden;
-  padding-bottom: ${theme.size.sm};
+  /* padding-bottom: ${theme.size.sm}; */
+
+  scrollbar-width: none;
+  scrollbar-color: transparent;
+  ::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+  }
 `;
