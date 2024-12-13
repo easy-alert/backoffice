87ef871d58d65e670d2bd2ff0533dde3d3dac8ed
Guilherme Roos <guilherme.roosr@gmail.com>
2024-12-13 11:09:34 -0300

    feat: enhance ChatWidget with user-specific input and theme styling

--- src/components/ChatWidget/index.tsx
+++ src/components/ChatWidget/index.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-namespace */
+import { useAuthContext } from '@contexts/Auth/UseAuthContext';
+
+import { theme } from '@styles/theme';
 import * as Style from './styles';
 
 declare global {
@@ -10,23 +13,34 @@ declare global {
 }
 
 export default function ChatWidget() {
+  const { user } = useAuthContext();
+
+  const chatTweaks = {
+    'TextInput-ATTke': {
+      input_value: user?.id,
+    },
+  };
+
   return (
     <Style.ChatWidgetContainer>
       <langflow-chat
         window_title="Izinho Chatbot"
         online_message="Izinho está online e pronto para ajudar!"
-        flow_id="5e60c5d6-f0f9-4924-96be-51e098931fd5"
-        host_url="https://guiroos-izinho-hmg.hf.space"
+        flow_id="ad64085d-4388-4d80-87f2-99d24350ff74"
+        host_url="https://guiroos-izinho-chatbot.hf.space"
         chat_position="top-left"
         height="500px"
         width="400px"
         placeholder="Digite sua mensagem..."
         placeholder_sending="Izinho está pensando..."
-        tweaks={{
-          'TextInput-3SaR0': {
-            input_value: 'guilherme',
-          },
-        }}
+        tweaks={JSON.stringify(chatTweaks)}
+        chat_trigger_style={JSON.stringify({ background: theme.color.primary })}
+        send_icon_style={JSON.stringify({
+          color: theme.color.primary,
+          stroke: theme.color.primary,
+        })}
+        bot_message_style={JSON.stringify({ color: 'white', background: theme.color.primary })}
+        user_message_style={JSON.stringify({ color: 'white', background: theme.color.gray4 })}
       />
     </Style.ChatWidgetContainer>
   );
