d257869184a4703a86c6d29b196ea60e33aaff43
Guilherme Roos <guilherme.roos@easyalert.com.br>
2025-02-17 22:33:57 -0300

    fix: update Vite build configuration to target 'esnext', enable minification with 'esbuild', and add sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   },
 
   build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: true,
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
