3969a987a40152437c8254585f38b7d7a9681c1a
Guilherme Roos <guilherme.roos@easyalert.com.br>
2025-02-17 21:41:04 -0300

    fix: configure Vite build options for chunk size warning and manual chunking

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,21 @@ import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+
+          return 'other';
+        }
+      }
+    },
+  },
+
   resolve: {
     alias: {
       '@assets': resolve(__dirname, './src/assets/'),
