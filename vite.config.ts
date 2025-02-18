23fc6e443e3cf25ab8922da81a577b34cd3f7f86
Guilherme Roos <guilherme.roos@easyalert.com.br>
2025-02-17 22:23:45 -0300

    fix: update Vite configuration and dependency for improved development experience

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 
 export default defineConfig({
+  server: {
+    port: 3002,
+  },
+
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
