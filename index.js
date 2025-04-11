2b926a472d7cbc506b88bf8bd95f13ede272592c
Guilherme Roos <guilherme@easyalert.com.br>
2025-04-11 11:46:41 -0300

    fix: downgrade express to version 4.21.2 and update related dependencies in package.json and package-lock.json

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const port = process.env.PORT || 8081;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Para rotas SPA (React Router)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
