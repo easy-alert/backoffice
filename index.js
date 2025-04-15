85ad2ffb540cf773271b663a5eca04cf8d13f6af
Guilherme Roos <guilherme@easyalert.com.br>
2025-04-15 18:00:14 -0300

    Add rate limiting middleware to enhance request handling

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 /* eslint-disable no-underscore-dangle */
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+  message: 'Muitas requisições feitas. Tente novamente mais tarde.',
+});
 
 const app = express();
 const port = process.env.PORT || 8081;
 
-// Servir arquivos estáticos (build do Vite)
-// app.use((req, res, next) => {
-//   if (/(.ico|.js|.css|.jpg|.png|.map|.svg)$/i.test(req.path)) {
-//       next();
-//   } else {
-//       res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-//       res.header('Expires', '-1');
-//       res.header('Pragma', 'no-cache');
-//       res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-//   }
-// });
-
-// app.use(express.static(path.join(__dirname, 'dist')))
-
+app.use(limiter);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
