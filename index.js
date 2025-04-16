dab6eea370c4794a0ccfc94a545cd37300d02113
Guilherme Roos <guilherme@easyalert.com.br>
2025-04-16 17:55:18 -0300

    fix: uncomment rate limiting setup and adjust max requests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import express from 'express';
-// import rateLimit from 'express-rate-limit';
+import rateLimit from 'express-rate-limit';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -10,17 +10,25 @@ const __dirname = dirname(__filename);
 const app = express();
 
 // Enable trust proxy
-// app.set('trust proxy', 1); // Trust first proxy
+app.set('trust proxy', 1); // Trust first proxy
 
 // Initialize the rate limiter
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 100, // max 100 requests per windowMs
-//   message: 'Muitas requisições feitas. Tente novamente mais tarde.',
-// });
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 200, // max 200 requests per windowMs
+  message: 'Muitas requisições feitas. Tente novamente mais tarde.', // message to send when rate limit is exceeded
+  skip: (req) => req.path !== '/login', // skip rate limiting for the login path
+  handler: (req, res, next, options) => {
+    console.warn(`Rate limit exceeded for ${req.ip} on ${req.path}`);
+
+    res.status(options.statusCode).json({
+      message: options.message,
+    });
+  },
+});
 
 // Apply the rate limiter to all requests
-// app.use(limiter);
+app.use(limiter);
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -31,7 +39,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
