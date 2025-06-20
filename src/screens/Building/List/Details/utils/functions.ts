cadd4cfa8179dcb681fc4c6686dda41e4afe3712
Marcelli <marcelli@easyalert.com.br>
2025-06-20 11:23:19 -0300

    fix: remove local functions and typings from BuildingDetails

--- src/screens/Building/List/Details/utils/functions.ts
+++
@@ -1,8 +0,0 @@
-import { Api } from '@services/api';
-import type { IBuildingDetails } from './types';
-
-export const requestBuildingDetails = async (buildingId: string): Promise<IBuildingDetails> => {
-  const uri = `/buildings/list/details/${buildingId}`;
-  const response = await Api.get(uri);
-  return response.data;
-};
