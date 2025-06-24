13025bad30a5b9aac8a69755d081c6abfb6e48ad
Marcelli <marcelli@easyalert.com.br>
2025-06-24 18:12:54 -0300

    feat: new typings

--- src/types/IUser.ts
+++ src/types/IUser.ts
@@ -4,4 +4,16 @@ export interface IUser {
   email: string;
   image?: string | null;
   lastAccess?: string;
+  role?: string;
+  phoneNumber?: string;
+}
+
+export interface IFormDataUser {
+  name: string;
+  image?: string;
+  role: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  confirmPassword: string;
 }
