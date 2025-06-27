ac1db57712a6b9695a10f195b47b93d385398b22
Marcelli <marcelli@easyalert.com.br>
2025-06-27 19:22:58 -0300

    fix: refactor function

--- src/services/apis/createUser.ts
+++ src/services/apis/createUser.ts
@@ -1,24 +1,15 @@
-import * as Yup from 'yup';
 import { Api } from '@services/api';
-import { catchHandler } from '@utils/functions';
-import { IFormDataUser } from '@customTypes/IUser';
+import { handleToastify } from '@utils/toastifyResponses';
 
-export const schemaUserCreate = Yup.object().shape({
-  name: Yup.string().required('Nome obrigatório'),
-  email: Yup.string().email('E-mail inválido').required('E-mail obrigatório'),
-  phoneNumber: Yup.string().required('Telefone obrigatório'),
-  password: Yup.string().min(6, 'Mínimo 6 caracteres').required('Senha obrigatória'),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password')], 'As senhas não coincidem')
-    .required('Confirme a senha'),
-});
+export async function createUser(data: any) {
+  const uri = 'users';
 
-export async function createUser(data: IFormDataUser) {
   try {
-    const response = await Api.post('account/users/create', data);
+    const response = await Api.post(uri, data);
+    handleToastify(response);
     return response.data;
-  } catch (error) {
-    catchHandler(error);
+  } catch (error: any) {
+    handleToastify(error.response);
     throw error;
   }
 }
