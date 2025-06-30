48017afc6ecd020b247214b1714e4a81fad021cd
Guilherme Roos <guilherme@easyalert.com.br>
2025-06-30 20:39:28 -0300

    refactor: reorganize theme structure and improve color definitions

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,35 +1,58 @@
 export const theme = {
   color: {
     primary: '#B21D1D',
-    success: '#34B53A',
-    danger: '#FF4E4E',
-
     primaryM: '#E66666',
     primaryL: '#F1A7A7',
 
+    success: '#34B53A',
     successM: '#74D779',
     successL: '#C4EEC6',
 
+    danger: '#FF4E4E',
     dangerM: '#FF6D4C',
     dangerL: '#FFC0B3',
     actionDanger: '#FF3508',
+    actionBlue: '#1976d2',
+    purple: '#8A1FDF',
 
+    warning: '#FFB200',
     warningL: '#FFE8B3',
+
     infoL: '#B3DFFF',
 
+    gray0: '#FAFAFA',
     gray1: '#EDEDED',
     gray2: '#E6E6E6',
     gray3: '#D5D5D5',
     gray4: '#999999',
     gray5: '#2B2B2B',
+    gray6: '#3F3E3E',
+
+    orange1: '#FF7B1C',
+
+    common: '#087EB4',
 
     black: '#000000',
     white: '#FFFFFF',
-  } as const,
+  },
+
+  background: {
+    open: '#FFFFFF',
+    pending: '#D5D5D5',
+    awaitingToFinish: '#FFB200',
+    inProgress: '#FFB200',
+
+    finished: '#34B53A',
+    completed: '#34B53A',
+
+    dismissed: '#B21D1D',
+  },
 
   size: {
     xxsm: '0.25rem', // 4px
     xsm: '0.5rem', // 8px
+    csm: '0.75rem', // 12px
+    csm2: '0.875rem', // 14px
     sm: '1rem', // 16px
     md: '1.5rem', // 24px
     lg: '2rem', // 32px
@@ -37,5 +60,5 @@ export const theme = {
     xxlg: '3rem', // 48px
     xxxlg: '3.5rem', // 56px
     xxxxlg: '4rem', // 64px
-  } as const,
+  },
 };
