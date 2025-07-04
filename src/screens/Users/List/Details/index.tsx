b779edefcf52c2a1d980d7ca858c4d6d0a107d30
Marcelli <marcelli@easyalert.com.br>
2025-07-04 08:25:50 -0300

    feat: add user enable/disable button

--- src/screens/Users/List/Details/index.tsx
+++ src/screens/Users/List/Details/index.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 
 // LIBS
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
 
 // GLOBAL COMPONENTS
 import { Tag } from '@components/Tag';
@@ -10,30 +10,39 @@ import { ReturnButton } from '@components/Buttons/ReturnButton';
 import { IconButton } from '@components/Buttons/IconButton';
 import { Image } from '@components/Image';
 import { DotSpinLoading } from '@components/Loadings/DotSpinLoading';
+import { PopoverButton } from '@components/Buttons/PopoverButton';
 
 // GLOBAL UTILS
 import { applyMask } from '@utils/functions';
 
 // GLOBAL STYLES
+import { theme } from '@styles/theme';
 import { icon } from '@assets/icons';
 import * as Style from './styles';
-
-// SERVICES
-import { fetchUserDetails, formatDate, formatDateTime } from './utils/functions';
 
 // TYPES
 import type { IUserDetails } from './utils/types';
 
 // MODALS
 import { ModalEditUser } from './modals/ModalEditUser';
+import {
+  fetchUserDetails,
+  formatDate,
+  formatDateTime,
+  updateUserBlockedStatus,
+} from './utils/functions';
 
 export const UserDetails = () => {
   const { userId } = useParams();
+  const location = useLocation();
   const [user, setUser] = useState<IUserDetails | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [modalEditUser, setModalEditUser] = useState<boolean>(false);
   const { search } = window.location;
   const navigate = useNavigate();
+  const [onQuery, setOnQuery] = useState(false);
+
+  const onUserUpdate = location.state?.onUserUpdate;
 
   useEffect(() => {
     const loadUserData = async () => {
@@ -44,7 +53,6 @@ export const UserDetails = () => {
         setUser(response.user);
         setLoading(false);
       } catch (error) {
-        console.error('Failed to load user data:', error);
         setLoading(false);
       }
     };
@@ -58,6 +66,35 @@ export const UserDetails = () => {
     }
   };
 
+  const requestChangeIsBlocked = async () => {
+    if (!user) return;
+    setOnQuery(true);
+
+    try {
+      const response = await updateUserBlockedStatus(user.id);
+
+      setUser((prev) =>
+        prev
+          ? {
+              ...prev,
+              isBlocked: response.isBlocked,
+            }
+          : prev,
+      );
+
+      if (onUserUpdate) {
+        onUserUpdate({
+          id: user.id,
+          isBlocked: response.isBlocked,
+        });
+      }
+    } catch (error) {
+      console.error('Erro ao alterar status do usuário:', error);
+    } finally {
+      setOnQuery(false);
+    }
+  };
+
   return (
     <>
       {modalEditUser && user && (
@@ -66,6 +103,16 @@ export const UserDetails = () => {
           handleModals={handleModals}
           onUserUpdated={(updatedUser) => {
             setUser(updatedUser as IUserDetails);
+
+            if (onUserUpdate) {
+              onUserUpdate({
+                id: updatedUser.id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                image: updatedUser.image,
+                isBlocked: updatedUser.isBlocked,
+              });
+            }
           }}
         />
       )}
@@ -120,12 +167,28 @@ export const UserDetails = () => {
             </Style.ProfileContent>
           </Style.ProfileSection>
 
-          <IconButton
-            hideLabelOnMedia
-            icon={icon.editWithBg}
-            label="Editar"
-            onClick={() => setModalEditUser(true)}
-          />
+          <Style.ActionsWrapper>
+            <PopoverButton
+              disabled={onQuery}
+              actionButtonBgColor={user?.isBlocked ? theme.color.success : theme.color.actionDanger}
+              type="IconButton"
+              label={user?.isBlocked ? 'Ativar' : 'Desativar'}
+              buttonIcon={user?.isBlocked ? icon.checked : icon.block}
+              message={{
+                title: `Deseja ${user?.isBlocked ? 'ativar' : 'desativar'} o acesso deste usuário?`,
+                content: 'Esta ação poderá ser desfeita posteriormente.',
+                contentColor: theme.color.danger,
+              }}
+              actionButtonClick={requestChangeIsBlocked}
+            />
+
+            <IconButton
+              hideLabelOnMedia
+              icon={icon.editWithBg}
+              label="Editar"
+              onClick={() => setModalEditUser(true)}
+            />
+          </Style.ActionsWrapper>
 
           <h2>Empresas vinculadas</h2>
 
