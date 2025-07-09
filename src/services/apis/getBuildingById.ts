83533a33e7484ae17f4fdde33c754b95720c5e12
Marcelli <marcelli@easyalert.com.br>
2025-07-09 10:30:12 -0300

    feat: search for details and types of buildings

--- src/services/apis/getBuildingById.ts
+++ src/services/apis/getBuildingById.ts
@@ -1,8 +1,14 @@
 import { IBuilding } from '@customTypes/IBuilding';
 import { Api } from '@services/api';
+import { IBuildingType } from '@utils/types';
 
 export const getBuildingById = async (buildingId: string): Promise<IBuilding> => {
   const uri = `/buildings/list/details/${buildingId}`;
   const response = await Api.get(uri);
+  return response.data;
+};
+
+export const getBuildingTypes = async (): Promise<IBuildingType[]> => {
+  const response = await Api.get('/buildings/types/list');
   return response.data;
 };
