7aca95d8f58df2238859e786452e76e3f8ef4b83
Marcelli <marcelli@easyalert.com.br>
2025-07-09 17:13:40 -0300

    fix: refactor api

--- src/services/apis/getBuildingById.ts
+++ src/services/apis/getBuildingById.ts
@@ -1,14 +1,15 @@
 import { IBuilding } from '@customTypes/IBuilding';
 import { Api } from '@services/api';
-import { IBuildingType } from '@utils/types';
 
-export const getBuildingById = async (buildingId: string): Promise<IBuilding> => {
+export interface IGetBuildingByIdProps {
+  buildingId: string;
+}
+
+export const getBuildingById = async ({
+  buildingId,
+}: IGetBuildingByIdProps): Promise<IBuilding> => {
   const uri = `/buildings/list/details/${buildingId}`;
-  const response = await Api.get(uri);
-  return response.data;
-};
 
-export const getBuildingTypes = async (): Promise<IBuildingType[]> => {
-  const response = await Api.get('/buildings/types/list');
+  const response = await Api.get(uri);
   return response.data;
 };
