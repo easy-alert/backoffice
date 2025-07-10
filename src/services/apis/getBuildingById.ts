d763eb2bc62ec5364fa8c915e99b1974fbebd182
Marcelli <marcelli@easyalert.com.br>
2025-07-10 11:48:52 -0300

    fix: standardizes function

--- src/services/apis/getBuildingById.ts
+++ src/services/apis/getBuildingById.ts
@@ -1,5 +1,6 @@
-import { IBuilding } from '@customTypes/IBuilding';
+import type { IBuilding } from '@customTypes/IBuilding';
 import { Api } from '@services/api';
+import { handleToastify } from '@utils/toastifyResponses';
 
 export interface IGetBuildingByIdProps {
   buildingId: string;
@@ -7,9 +8,14 @@ export interface IGetBuildingByIdProps {
 
 export const getBuildingById = async ({
   buildingId,
-}: IGetBuildingByIdProps): Promise<IBuilding> => {
+}: IGetBuildingByIdProps): Promise<IBuilding | null> => {
   const uri = `/buildings/list/details/${buildingId}`;
 
-  const response = await Api.get(uri);
-  return response.data;
+  try {
+    const response = await Api.get(uri);
+    return response.data;
+  } catch (error: any) {
+    handleToastify(error.response);
+    return null;
+  }
 };
--- src/services/apis/getBuildingTypes.ts
+++ src/services/apis/getBuildingTypes.ts
@@ -1,9 +1,15 @@
 import { Api } from '@services/api';
 import type { IBuildingType } from '@utils/types';
+import { handleToastify } from '@utils/toastifyResponses';
 
 export const getBuildingTypes = async (): Promise<IBuildingType[]> => {
   const uri = '/buildings/types/list';
 
-  const response = await Api.get(uri);
-  return response.data;
+  try {
+    const response = await Api.get(uri);
+    return response.data;
+  } catch (error: any) {
+    handleToastify(error.response);
+    return [];
+  }
 };
