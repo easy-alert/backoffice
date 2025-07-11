5c908c950ffa024ca25d043848df874758c53393
Marcelli <marcelli@easyalert.com.br>
2025-07-10 22:44:04 -0300

    fix:uses the unMask function to clear the CEP when sending the building edition

--- src/services/apis/putEditBuilding.ts
+++ src/services/apis/putEditBuilding.ts
@@ -1,12 +1,13 @@
 import type { IUpdateBuildingData } from '@customTypes/IBuilding';
 import { Api } from '@services/api';
 import { handleToastify } from '@utils/toastifyResponses';
+import { unMask } from '@utils/functions';
 
 export async function putEditBuilding(buildingId: string, values: any) {
   const buildingData: Partial<IUpdateBuildingData> = {
     name: values.name?.trim() || '',
     buildingTypeId: values.buildingTypeId || '',
-    cep: values.cep?.replace(/\D/g, '') || '',
+    cep: values.cep ? unMask(values.cep) : '',
     state: values.state?.trim() || '',
     city: values.city?.trim() || '',
     warrantyExpiration: values.warrantyExpiration ? new Date(values.warrantyExpiration) : undefined,
