35e6ef4f08b92fa00dd6f9ef39c4663944d38deb
Marcelli <marcelli@easyalert.com.br>
2025-07-16 11:45:26 -0300

    feat:modal to register guarantee

---
+++ src/screens/Guarantees/components/ModalRegisterGuaratee/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+  padding: 24px 0 0 0;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+`;
