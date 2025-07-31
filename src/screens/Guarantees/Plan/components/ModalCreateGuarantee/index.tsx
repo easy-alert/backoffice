246fc9a179dae2aa296e44e8520935728a38204a
Guilherme Roos <guilherme@easyalert.com.br>
2025-07-31 15:11:52 -0300

    feat: implements hooks from listing of the modal form

--- src/screens/Guarantees/Plan/components/ModalCreateGuarantee/index.tsx
+++ src/screens/Guarantees/Plan/components/ModalCreateGuarantee/index.tsx
@@ -5,25 +5,28 @@ import { useState } from 'react';
 import { Form, Formik } from 'formik';
 import * as Yup from 'yup';
 
+// HOOKS
+import { useGuaranteeSystemForSelect } from '@hooks/useGuaranteeSystemForSelect';
+import { useGuaranteeFailureTypesForSelect } from '@hooks/useGuaranteeFailureTypesForSelect';
+
 // GLOBAL COMPONENTS
+import { DotSpinLoading } from '@components/Loadings/DotSpinLoading';
 import { Button } from '@components/Buttons/Button';
 import { Modal } from '@components/Modal';
 import { FormikInput } from '@components/Form/FormikInput';
 import { FormikSelect } from '@components/Form/FormikSelect';
 import { FormikError } from '@components/Form/FormikError';
+import { FormikTextArea } from '@components/Form/FormikTextArea';
 import { ReactSelectComponent } from '@components/ReactSelectComponent';
 
 // GLOBAL TYPES
-import type { IGuaranteeSystem } from '@customTypes/IGuaranteeSystem';
-import type { IGuaranteeFailureType } from '@customTypes/IGuaranteeFailureType';
 import type { ICreateGuaranteePlan } from '@services/apis/createGuaranteePlan';
 
 // STYLES
 import * as Style from './styles';
 
 interface IModalCreateGuarantee {
-  systems: IGuaranteeSystem[];
-  failureTypes: IGuaranteeFailureType[];
+  selectedSystemId?: string;
   handleCreateGuaranteePlan: (values: ICreateGuaranteePlan) => Promise<void>;
   handleModalCreateGuarantee: (state: boolean) => void;
 }
@@ -31,6 +34,7 @@ interface IModalCreateGuarantee {
 const schema = Yup.object().shape({
   systemId: Yup.string().required('Sistema obrigatório'),
   description: Yup.string().required('Descrição obrigatória'),
+  observation: Yup.string().optional(),
   failureTypesIds: Yup.array()
     .of(Yup.string())
     .test('at-least-one-failure', 'Pelo menos uma falha deve ser selecionada', (value) =>
@@ -42,19 +46,31 @@ const schema = Yup.object().shape({
 });
 
 export const ModalCreateGuarantee = ({
-  systems,
-  failureTypes,
+  selectedSystemId,
   handleCreateGuaranteePlan,
   handleModalCreateGuarantee,
 }: IModalCreateGuarantee) => {
+  const { guaranteeSystemsForSelect, loadingGuaranteeSystemsForSelect } =
+    useGuaranteeSystemForSelect({
+      companyId: '',
+      getDefault: true,
+    });
+
+  const { guaranteeFailureTypesForSelect, loadingGuaranteeFailureTypesForSelect } =
+    useGuaranteeFailureTypesForSelect({
+      companyId: '',
+      getDefault: true,
+    });
+
   const [onQuery, setOnQuery] = useState(false);
 
   return (
     <Modal title="Cadastrar garantia" setModal={handleModalCreateGuarantee}>
       <Formik
         initialValues={{
-          systemId: '',
+          systemId: selectedSystemId || '',
           description: '',
+          observation: '',
           failureTypesIds: [],
           warrantyPeriod: 1,
           warrantyPeriodUnit: 'year',
@@ -66,6 +82,7 @@ export const ModalCreateGuarantee = ({
           await handleCreateGuaranteePlan({
             systemId: values.systemId,
             description: values.description,
+            observation: values.observation,
             failureTypesIds: values.failureTypesIds,
             warrantyPeriod:
               values.warrantyPeriodUnit === 'year'
@@ -80,95 +97,114 @@ export const ModalCreateGuarantee = ({
       >
         {({ errors, values, touched, setFieldValue }) => (
           <Style.FormContainer>
-            <Form>
-              <FormikSelect
-                label="Sistema *"
-                name="systemId"
-                value={values.systemId}
-                selectPlaceholderValue={values.systemId}
-                error={touched.systemId && errors.systemId ? errors.systemId : null}
-              >
-                <option value="" disabled hidden>
-                  Selecione
-                </option>
+            {loadingGuaranteeSystemsForSelect && loadingGuaranteeFailureTypesForSelect && (
+              <DotSpinLoading />
+            )}
 
-                {systems &&
-                  systems.map((system) => (
-                    <option key={system.id} value={system.id}>
-                      {system.name}
-                    </option>
-                  ))}
-              </FormikSelect>
-
-              <FormikInput
-                name="description"
-                label="Descrição *"
-                placeholder="Ex: Garantia de 1 ano"
-                value={values.description}
-                error={touched.description && errors.description ? errors.description : null}
-              />
-
-              <div>
-                <ReactSelectComponent
-                  selectPlaceholderValue={values.failureTypesIds?.length}
-                  isMulti
-                  id="failureTypesIds"
-                  name="failureTypesIds"
-                  placeholder="Selecione um ou mais falhas"
-                  label="Falha(s) *"
-                  options={failureTypes.map(({ id, name }) => ({ label: name, value: id }))}
-                  value={values.failureTypesIds
-                    ?.map((failureTypeId) => {
-                      const failureType = failureTypes.find(({ id }) => id === failureTypeId);
-                      return failureType
-                        ? { label: failureType.name, value: failureType.id }
-                        : null;
-                    })
-                    .filter(Boolean)}
-                  onChange={(e) => {
-                    const selectedValues = e.map((option: { value: string }) => option.value);
-                    setFieldValue('failureTypesIds', selectedValues);
-                  }}
-                />
-
-                {errors.failureTypesIds && touched.failureTypesIds && (
-                  <FormikError name="failureTypesIds" />
-                )}
-              </div>
-
-              <div style={{ display: 'flex', gap: '1rem' }}>
-                <FormikInput
-                  name="warrantyPeriod"
-                  label="Prazo*"
-                  type="number"
-                  value={values.warrantyPeriod}
-                  error={
-                    touched.warrantyPeriod && errors.warrantyPeriod ? errors.warrantyPeriod : null
-                  }
-                />
-
+            {!loadingGuaranteeSystemsForSelect && !loadingGuaranteeFailureTypesForSelect && (
+              <Form>
                 <FormikSelect
-                  name="warrantyPeriodUnit"
-                  label="Unidade*"
-                  selectPlaceholderValue={values.warrantyPeriodUnit}
-                  value={values.warrantyPeriodUnit}
-                  error={
-                    touched.warrantyPeriodUnit && errors.warrantyPeriodUnit
-                      ? errors.warrantyPeriodUnit
-                      : null
-                  }
+                  label="Sistema *"
+                  name="systemId"
+                  value={values.systemId}
+                  selectPlaceholderValue={values.systemId}
+                  error={touched.systemId && errors.systemId ? errors.systemId : null}
                 >
                   <option value="" disabled hidden>
                     Selecione
                   </option>
 
-                  <option value="year">Anos</option>
-                  <option value="month">Meses</option>
+                  {guaranteeSystemsForSelect &&
+                    guaranteeSystemsForSelect.map((system) => (
+                      <option key={system.id} value={system.id}>
+                        {system.name}
+                      </option>
+                    ))}
                 </FormikSelect>
-              </div>
 
-              <Button center label="Cadastrar" type="submit" loading={onQuery} />
-            </Form>
+                <FormikInput
+                  name="description"
+                  label="Descrição *"
+                  placeholder="Ex: Garantia de 1 ano"
+                  value={values.description}
+                  error={touched.description && errors.description ? errors.description : null}
+                />
+
+                <FormikTextArea
+                  name="observation"
+                  label="Observação"
+                  placeholder="Ex: Observação"
+                  value={values.observation}
+                  error={touched.observation && errors.observation ? errors.observation : null}
+                />
+
+                <div>
+                  <ReactSelectComponent
+                    selectPlaceholderValue={values.failureTypesIds?.length}
+                    isMulti
+                    id="failureTypesIds"
+                    name="failureTypesIds"
+                    placeholder="Selecione um ou mais falhas"
+                    label="Falha(s) *"
+                    options={guaranteeFailureTypesForSelect.map(({ id, name }) => ({
+                      label: name,
+                      value: id,
+                    }))}
+                    value={values.failureTypesIds
+                      ?.map((failureTypeId) => {
+                        const failureType = guaranteeFailureTypesForSelect.find(
+                          ({ id }) => id === failureTypeId,
+                        );
+                        return failureType
+                          ? { label: failureType.name, value: failureType.id }
+                          : null;
+                      })
+                      .filter(Boolean)}
+                    onChange={(e) => {
+                      const selectedValues = e.map((option: { value: string }) => option.value);
+                      setFieldValue('failureTypesIds', selectedValues);
+                    }}
+                  />
+
+                  {errors.failureTypesIds && touched.failureTypesIds && (
+                    <FormikError name="failureTypesIds" />
+                  )}
+                </div>
+
+                <div style={{ display: 'flex', gap: '1rem' }}>
+                  <FormikInput
+                    name="warrantyPeriod"
+                    label="Prazo*"
+                    type="number"
+                    value={values.warrantyPeriod}
+                    error={
+                      touched.warrantyPeriod && errors.warrantyPeriod ? errors.warrantyPeriod : null
+                    }
+                  />
+
+                  <FormikSelect
+                    name="warrantyPeriodUnit"
+                    label="Unidade*"
+                    selectPlaceholderValue={values.warrantyPeriodUnit}
+                    value={values.warrantyPeriodUnit}
+                    error={
+                      touched.warrantyPeriodUnit && errors.warrantyPeriodUnit
+                        ? errors.warrantyPeriodUnit
+                        : null
+                    }
+                  >
+                    <option value="" disabled hidden>
+                      Selecione
+                    </option>
+
+                    <option value="year">Anos</option>
+                    <option value="month">Meses</option>
+                  </FormikSelect>
+                </div>
+
+                <Button center label="Cadastrar" type="submit" loading={onQuery} />
+              </Form>
+            )}
           </Style.FormContainer>
         )}
       </Formik>
